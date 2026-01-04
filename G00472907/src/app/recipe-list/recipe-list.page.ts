40505c5124b90301620cc6412583701d39b8c673
akirby23 <aoifekirby33@gmail.com>
2026-01-04 15:54:31 +0000

    Add text to display when no recipes are found

--- G00472907/src/app/recipe-list/recipe-list.page.ts
+++ G00472907/src/app/recipe-list/recipe-list.page.ts
@@ -2,14 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonImg, IonItem } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonImg, IonItem, IonText } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.page.html',
   styleUrls: ['./recipe-list.page.scss'],
   standalone: true,
-  imports: [IonItem, IonImg, IonButton, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent,]
+  imports: [IonItem, IonImg, IonButton, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonText]
 })
 export class RecipeListPage  {
   constructor(private router: Router) {}
